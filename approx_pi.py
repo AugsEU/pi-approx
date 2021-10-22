4184f48798ccb1bafbd1c695020e16593d32fa26
adahlkvist <AugustDahlkvist@gmail.com>
2021-10-22 23:38:01 +0100

    Show actual values with clear descriptions

--- approx_pi.py
+++ approx_pi.py
@@ -9,7 +9,6 @@ for i in range(10):
     pi_a = 2**(i+2) * 3 * a
     pi_b = 2**(i+3) * b
     print("=====")
-    print((pi_a/math.pi)-1)
-    print((pi_b/math.pi)-1)
-    print(0.5*(pi_b+pi_a)/math.pi -1)
-    print(math.pi)
+    print("Lower: " + str(pi_a))
+    print("Upper: " + str(pi_b))
+    print("Real: " + str(math.pi))
